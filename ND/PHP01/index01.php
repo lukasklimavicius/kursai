e4771b091ad614ee2a923671e9bb05df5c87f2b1
lukasklimavicius <48210467+lukasklimavicius@users.noreply.github.com>
2019-03-25 20:40:42 +0200

    Create index01.php

---
+++ ND/PHP01/index01.php
@@ -0,0 +1,22 @@
+<?php 
+    $šalis = 'Lietuva';
+    $miestas = 'Vilnius';
+    $populiacija = 574221;
+?>
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 01</title>
+</head>
+
+<body>
+    <ul>
+        <li><?php echo $šalis; ?></li>
+        <li><?php echo $miestas; ?></li>
+        <li><?php echo $populiacija; ?></li>
+    </ul>
+</body>
+    
+</html>
