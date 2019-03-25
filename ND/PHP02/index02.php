f38010519c40482366b318c62ff74a65d427e9ab
lukasklimavicius <48210467+lukasklimavicius@users.noreply.github.com>
2019-03-25 20:48:12 +0200

    Create index02.php

---
+++ ND/PHP02/index02.php
@@ -0,0 +1,19 @@
+<?php 
+    $šalis = 'Lietuva';
+    $miestas = 'Vilnius';
+    $populiacija = 574221;
+?>
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 02</title>
+</head>
+
+<body>
+    <?php echo $miestas.' - Lietuvos sostinė';?>
+    <?php echo "$miestas ir $šalis sparčiai auga";?>
+</body>
+    
+</html>
