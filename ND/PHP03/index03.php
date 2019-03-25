4c3a82a46098b433df4d8d962767282b6932bc0d
lukasklimavicius <48210467+lukasklimavicius@users.noreply.github.com>
2019-03-25 20:57:41 +0200

    Create index03.php

---
+++ ND/PHP03/index03.php
@@ -0,0 +1,15 @@
+<?php 
+    $x = 10;
+    $y = 7;
+    $ats = $x+$y;
+    echo $x. '+'. $y. '='. $ats. '<br>';
+    $ats = $x-$y;
+    echo $x. '-'. $y. '='. $ats. '<br>';
+    $ats = $x*$y;
+    echo $x. '*'. $y. '='. $ats. '<br>';
+    $ats = $x/$y;
+    echo $x. '/'. $y. '='. $ats. '<br>';
+    $ats = $x%$y;
+    echo $x. '%'. $y. '='. $ats;
+?>
+
