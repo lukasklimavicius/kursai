1ed569cae98798de4d259b188066ef1c788ea93c
lukasklimavicius <48210467+lukasklimavicius@users.noreply.github.com>
2019-03-28 21:08:25 +0200

    Update index.php

--- ND/PHP11/index.php
+++ ND/PHP11/index.php
@@ -5,7 +5,7 @@ if (isset($_GET['ilgis']) && isset($_GET['plotis'])) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="">
+<html lang="lt">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
