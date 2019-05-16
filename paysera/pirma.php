42bde4915bbab6de45ff2a892ac92bbf3d092fca
lukasklimavicius <48210467+lukasklimavicius@users.noreply.github.com>
2019-05-16 17:58:29 +0300

    Update pirma.php

--- paysera/pirma.php
+++ paysera/pirma.php
@@ -1,15 +1,18 @@
 <?php
     $rezultatai = array("3:1", "2:2", "0:1", "0:3", "1:3", "2:0", "2:1", "0:0", "1:0", "3:1");
-    $str = implode("", $rezultatai);
-    $taskai = 0;
-    $kiek = (count($rezultatai)-1)*3;
-    for ($i=0; $i<=$kiek; $i+=3) {
-        $j=$i;
-        if ($str[$j]>$str[$j+2]) {
-            $taskai+=3;
-        } elseif ($str[$j]==$str[$j+2]) {
-            $taskai+=1;
+    function taskuSkaiciavimas ($rezultatai) {
+        $str = implode("", $rezultatai);
+        $taskai = 0;
+        $kiek = (count($rezultatai)-1)*3;
+        for ($i=0; $i<=$kiek; $i+=3) {
+            $j=$i;
+            if ($str[$j]>$str[$j+2]) {
+                $taskai+=3;
+            } elseif ($str[$j]==$str[$j+2]) {
+                $taskai+=1;
+            }
         }
+        echo "Taškų suma: $taskai";
     }
-    echo "Taškų suma: $taskai";
+    taskuSkaiciavimas($rezultatai);
 ?>
